0f4e057a573c453b73a669637a19e242ee109cb6
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-11-11 23:31:46 +0100

    reorder app folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Business from './components/Business/Business'
 import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
 
@@ -8,6 +9,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>ravenous</h1>
+        <Business />
         <SearchBar />
         <BusinessList />
       </div>      
